08fb58ab0fa6569db1d7518759503c69aa8bf23f
ritsadhil <haritsfadhil884@gmail.com>
2025-11-30 21:20:52 +0700

    kinda cooking

---
+++ pertemuan3/latihan/latihan/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+// Contoh route untuk menampilkan view welcome
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/about', function () {
+    return view('about');
+});
